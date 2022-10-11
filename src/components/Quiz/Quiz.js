a3a9703fd665f2231886c773cbb5a3f548127272
Farjana Islam <farjanaanika2@gmail.com>
2022-10-12 05:28:16 +0600

    quiz

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Quiz = ({questions}) => {
-    const {correctAnswer,id,name,question, options} = questions;
-//   const handleRadioBtn =
+    const {correctAnswer,question, options} = questions;
+  const handleRadioBtn = (option) =>{
+    if(option === correctAnswer){
+        console.log('right')
+    }
+    else{
+        <div className='toast alert alert-info'>
+            <span>Wrong Answer</span>
+        </div>
+    
+    }
+  }
     return (
         <div className='bg-pink-100 mt-10 ml-40 mr-40 p-10 rounded-md'>
             
@@ -12,14 +22,16 @@ const Quiz = ({questions}) => {
     {
         question.slice(3, -4)
     }
+    
     <EyeIcon className="h-8 w-8 text-dark"/>
     </div>
             <div className='text-lg grid grid-cols-2'>
          {
             options.map(option => <label  className='flex'>
-            <input className='' type="radio" name="" id="" />
-            <span className='grid grid-cols-2 ml-5 justify-center align-middle'>{option}</span>
+            <input className='' type="radio" name={'...id'} id="" />
+            <span onClick={() =>handleRadioBtn(option)} className='grid grid-cols-2 ml-5 justify-center align-middle'>{option}</span>
           </label>)
+          
          }
            
             </div>
